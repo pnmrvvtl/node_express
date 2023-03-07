55b83a800904dda247b998221f142f6ca16d0289
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-03-08 00:26:50 +0200

    feat: add schema and CRUD for task

--- task_manager_api/controllers/tasks.js
+++ task_manager_api/controllers/tasks.js
@@ -1,21 +1,51 @@
-const getAllTasks = (req, res) => {
-    res.send('all items');
+const Task = require('../models/task');
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({tasks});
+    } catch (e) {
+        res.status(500).json({msg: e});
+    }
 }
 
-const getTask = (req, res) => {
-    res.send('get task');
+const getTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOne({_id: taskID});
+        if (!task) {
+            return res.status(404).json({msg: `No task with id: ${taskID}`});
+        }
+        res.status(200).json({task});
+    } catch (e) {
+        res.status(500).json({msg: e});
+    }
 }
 
-const createTask = (req, res) => {
-    res.json(req.body);
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({task});
+    } catch (e) {
+        res.status(500).json({msg: e});
+    }
 }
 
 const updateTask = (req, res) => {
     res.send('update');
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete');
+const deleteTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id: taskID});
+        if (!task) {
+            return res.status(404).json({msg: `No task with id: ${taskID}`});
+        }
+        res.status(200).json({task});
+    } catch (e) {
+        res.status(500).json({msg: e});
+    }
 }
 
 
