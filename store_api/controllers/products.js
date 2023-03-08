545ba6349a70407e53f6b22186d1c6c820518ce5
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-03-09 01:05:15 +0200

    feat: add query params for products controller

--- store_api/controllers/products.js
+++ store_api/controllers/products.js
@@ -1,9 +1,27 @@
-const getAllProducts = (req, res) => {
-    return res.send('get all products');
+const Product = require('../models/product');
+
+const getAllProducts = async (req, res) => {
+    const {featured, company, name} = req.query;
+
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === 'true';
+    }
+    if (company) {
+        queryObject.company = company;
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i'}
+    }
+
+    const products = await Product.find(queryObject);
+    return res.status(200).json({products, nbHits: products.length});
 }
 
-const getAllProductsStatic = (req, res) => {
-    return res.send('get all products static');
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find();
+    return res.status(200).json({products, nbHits: products.length});
 }
 
 module.exports = {getAllProducts, getAllProductsStatic};
