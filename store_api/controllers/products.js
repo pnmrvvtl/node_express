fe5af40eff898c8c287f03436ce5bf9366ede035
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-03-09 14:02:05 +0200

    feat: create store_api project

--- store_api/controllers/products.js
+++ store_api/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 const getAllProducts = async (req, res) => {
-    const {featured, company, name} = req.query;
+    const {featured, company, name, sort, fields, numericFilters} = req.query;
 
     const queryObject = {};
 
@@ -12,10 +12,47 @@ const getAllProducts = async (req, res) => {
         queryObject.company = company;
     }
     if (name) {
-        queryObject.name = { $regex: name, $options: 'i'}
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+        }
+        const regEx = /\b(>|>=|=|<|<=)\b/g;
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+        const options = ['price', 'rating'];
+        filters.split(',').forEach((el) => {
+            const [field, operator, value] = el.split('-');
+            if (options.includes(field)) {
+                queryObject[field] = {[operator]: Number(value)}
+            }
+        })
     }
 
-    const products = await Product.find(queryObject);
+    let result = Product.find(queryObject);
+
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createdAt');
+    }
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const products = await result;
+
     return res.status(200).json({products, nbHits: products.length});
 }
 
